fbc0fbb2ab4287e4c8a482f44d7f8a84584ac398
Adam Troiani <adam.troiani@shopify.com>
2025-06-03 16:24:33 -0400

    rustlings ch 11

--- rust/rustlings/exercises/quizzes/quiz2.rs
+++ rust/rustlings/exercises/quizzes/quiz2.rs
@@ -27,7 +27,19 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        input
+            .iter()
+            .map(|(s, c)| match c {
+                Command::Uppercase => s.to_uppercase(),
+                Command::Trim => s.trim().to_string(),
+                Command::Append(n) => {
+                    let bars = "bar".repeat(*n);
+                    format!("{s}{bars}")
+                }
+            })
+            .collect()
+    }
 }
 
 fn main() {
@@ -36,9 +48,8 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
     use super::Command;
+    use crate::my_module::transformer;
 
     #[test]
     fn it_works() {
